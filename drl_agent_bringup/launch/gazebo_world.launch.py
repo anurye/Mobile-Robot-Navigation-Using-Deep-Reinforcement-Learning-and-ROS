8044c88f0af23c3ed4784a5b7a1f0d503270084b
Ahmed Yesuf Nurye <anurye.yesuf@gmail.com>
2024-07-27 22:55:57 +0200

    add Gazebo world file with actors

--- drl_agent_bringup/launch/gazebo_world.launch.py
+++ drl_agent_bringup/launch/gazebo_world.launch.py
@@ -4,8 +4,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_prefix
 
@@ -15,22 +16,28 @@ ARGUMENTS = [
                           choices=['true', 'false'],
                           description='Whether or not to launch gzclient'),
     DeclareLaunchArgument('world_path',
-          default_value=[os.path.join(get_package_share_directory('drl_agent_bringup'), 'worlds', 'TD3.world')],
+          default_value=[os.path.join(get_package_share_directory('drl_agent_bringup'), 'worlds', 'td7.world')],
           description='SDF world file'),
 ]
 
 
 def generate_launch_description():
 
-    # Get packages description and directory
+    """*************************************************************************************
+    ** Get package share directories
+    *************************************************************************************"""
     drl_agent_bringup_package_name = 'drl_agent_bringup'
     drl_agent_description_package_name = 'drl_agent_description'
     velodyne_description_package_name = 'velodyne_description'
     drl_agent_bringup_package_directory = get_package_share_directory(drl_agent_bringup_package_name)
     drl_agent_description_package_directory = get_package_share_directory(drl_agent_description_package_name)
     velodyne_description_package_directory = get_package_share_directory(velodyne_description_package_name)
+    gazebo_ros_package_directory = get_package_share_directory('gazebo_ros')
 
-    # Set the Path to mesh models. NOTE: should be done before gazebo is 1st launched.
+    """*************************************************************************************
+    ** Set the Path to mesh models. NOTE: should be done before gazebo is 1st launched.
+    *************************************************************************************"""
+    drl_agent_bringup_install_dir_path = (get_package_prefix(drl_agent_bringup_package_name) + '/share')
     drl_agent_install_dir_path = (get_package_prefix(drl_agent_description_package_name) + '/share')
     velodyne_description_install_dir_path = (get_package_prefix(velodyne_description_package_name) + '/share')
 
@@ -39,7 +46,7 @@ def generate_launch_description():
     drl_agent_description_models_path = os.path.join(drl_agent_description_package_directory, 'models')
     drl_agent_bringup_models_path = os.path.join(drl_agent_bringup_package_directory, 'models')
     
-    gazebo_resource_paths = [drl_agent_install_dir_path, robot_meshes_path, drl_agent_description_models_path,\
+    gazebo_resource_paths = [drl_agent_bringup_install_dir_path, drl_agent_install_dir_path, robot_meshes_path, drl_agent_description_models_path,\
                              drl_agent_bringup_models_path, velodyne_description_install_dir_path, velodyne_description_meshes_path]
     if 'GAZEBO_MODEL_PATH' in os.environ:
         for resource_path in gazebo_resource_paths:
@@ -57,31 +64,30 @@ def generate_launch_description():
     # print(str(os.environ['GAZEBO_PLUGIN_PATH']))
     print(border)
 
-    # Launch configurations
+    """*************************************************************************************
+    Launch configurations and gazebo launch
+    *************************************************************************************"""
     world_path = LaunchConfiguration('world_path')
     use_gazebo_gui = LaunchConfiguration('use_gazebo_gui')
 
-    # Gazebo server
-    gazebo_params_yaml_file = os.path.join(
-        drl_agent_bringup_package_directory, 'config', 'gazebo_params.yaml')
-    gzserver = ExecuteProcess(
-        cmd=['gzserver',
-             '-s', 'libgazebo_ros_init.so',
-             '-s', 'libgazebo_ros_factory.so',
-             world_path,
-             'extra-gazebo-args', '--ros-args', '--params-file', gazebo_params_yaml_file],
-        output='screen',
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros_package_directory, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments=[
+            ('world', world_path),
+        ]
     )
 
-    # Gazebo client
-    gzclient = ExecuteProcess(
-        cmd=['gzclient'],
-        output='screen',
-        condition=IfCondition(use_gazebo_gui),
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros_package_directory, 'launch', 'gzclient.launch.py')
+        ),
+        condition=IfCondition(use_gazebo_gui)
     )
 
     ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(gzserver)
-    ld.add_action(gzclient)
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
 
     return ld
